5479e7ca008ff112140942713fd8442283b5ced8
Stephen Chu <stephen.chu530@gmail.com>
2019-07-04 17:32:48 -0700

    feat: setup react and basic file and dir structure

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './header.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <p>Tasks</p>
+        <p>About</p>
+      </>
+    )
+  }
+}
+
+export default Header;
